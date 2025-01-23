f7a218285639107fc083033997497bd0ff13d624
ahmedelbagory332 <ahmedelbagory63@gmail.com>
2025-01-23 16:24:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigation_compose_type_safe/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.navigation_compose_type_safe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigation_compose_type_safe.ui.theme.Navigation_Compose_Type_SafeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Navigation_Compose_Type_SafeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Navigation_Compose_Type_SafeTheme {
+        Greeting("Android")
+    }
+}
